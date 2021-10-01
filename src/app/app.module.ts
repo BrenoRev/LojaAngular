a7038677de35a5f794553a5d6ed348e57c165ff6
BrenoRev <silvabreno462@gmail.com>
2021-10-01 00:56:18 -0300

    Componente Home e Navegação Entre Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,17 @@ import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component'
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
-
+import { HomeComponent } from './views/home/home.component';
+import { MatCardModule } from '@angular/material/card';
+import { ProductComponent } from './views/product/product.component'
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +28,8 @@ import { MatListModule } from '@angular/material/list';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
