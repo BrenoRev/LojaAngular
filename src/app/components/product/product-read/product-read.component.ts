d6c0ea5532541e21e12bf8d24bd260e7bc835cd7
BrenoRev <silvabreno462@gmail.com>
2021-10-04 22:33:23 -0300

    Criando o botão de alterar e deletar produto

--- src/app/components/product/product-read/product-read.component.ts
+++ src/app/components/product/product-read/product-read.component.ts
@@ -11,7 +11,7 @@ import { Component, OnInit } from '@angular/core';
 export class ProductReadComponent implements OnInit {
 
   products : Product[] = []
-  displayedColumns = ['id','name','price'];
+  displayedColumns = ['id','name','price','action'];
 
   constructor(private productService: ProductService) { }
 
